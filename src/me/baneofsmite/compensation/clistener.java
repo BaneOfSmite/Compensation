6439a1182a120052b4ff010d279e3bafc24d5a74
BaneOfSmite <BaneOfSmite@gmail.com>
2018-05-24 16:23:31 +0800

    Update clistener.java

--- src/me/baneofsmite/compensation/clistener.java
+++ src/me/baneofsmite/compensation/clistener.java
@@ -15,32 +15,35 @@ import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scoreboard.Team;
 
 public class clistener implements Listener {
-    @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent event) {
-        Player player = event.getEntity();
-        Team team = Bukkit.getScoreboardManager().getMainScoreboard().getEntryTeam(player.getName());
-        if (team != null) {
-            team.removeEntry(player.getName());
-            double compensation = player.getMaxHealth() / (double)team.getSize();
-            for (String teammate : team.getEntries()) {
-            	Player p = Bukkit.getServer().getPlayer(teammate);
-                new Compensator(p, compensation).runTaskTimer((Plugin)this, 0, 5);
-            }
-        }
-        player.setMaxHealth(20.0);
-    }
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event) {
+		Player player = event.getEntity();
+		Team team = Bukkit.getScoreboardManager().getMainScoreboard().getEntryTeam(player.getName());
+		if (Main.getCompensationv()) {
+			if (team != null) {
+				team.removeEntry(player.getName());
+				double compensation = player.getMaxHealth() / (double) team.getSize();
+				for (String teammate : team.getEntries()) {
+					Player p = Bukkit.getServer().getPlayer(teammate);
+					new Compensator(p, compensation).runTaskTimer((Plugin) this, 0, 5);
+				}
+			}
+		}
+		player.setMaxHealth(20.0);
+	}
 
-    @EventHandler
-    public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
-        Player player = event.getPlayer();
-        ItemStack item = event.getItem();
-        if (item.getType() == Material.GOLDEN_APPLE) {
-            double health = player.getMaxHealth() / 5.0;
-            int hearts = (int)health;
-            player.setHealth(player.getHealth() + health - (double)hearts);
-            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, hearts * 25, 1), true);
-        }
-    }
-    
+	@EventHandler
+	public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
+		Player player = event.getPlayer();
+		ItemStack item = event.getItem();
+		if (Main.getCompensationv()) {
+			if (item.getType() == Material.GOLDEN_APPLE) {
+				double health = player.getMaxHealth() / 5.0;
+				int hearts = (int) health;
+				player.setHealth(player.getHealth() + health - (double) hearts);
+				player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, hearts * 25, 1), true);
+			}
+		}
+	}
+
 }
-
